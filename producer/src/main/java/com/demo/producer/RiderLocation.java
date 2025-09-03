901b15883f6c11361a0e7b716ecb7a78b7be3667
vatsalchaudhary <vatsalchaudhary01@gmail.com>
2025-09-03 16:43:08 +0000

    Kafka Consumer and producer

---
+++ producer/src/main/java/com/demo/producer/RiderLocation.java
@@ -0,0 +1,37 @@
+package com.demo.producer;
+
+public class RiderLocation {
+    private String id;
+    private double latitude;
+    private double longitude;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public RiderLocation(String id, double latitude, double longitude) {
+        this.id = id;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
